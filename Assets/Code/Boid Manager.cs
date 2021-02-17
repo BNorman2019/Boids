b9edf24d82a8d41ee44f3360aceffb70d075e05c
BNorman2019 <54329156+BNorman2019@users.noreply.github.com>
2021-02-17 16:43:11 -0600

    Initial commit

---
+++ Assets/Code/Boid Manager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoidManager : MonoBehaviour
+{
+    public List<Boid> boids;
+    public int numBoids = 10;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (numBoids; numBoids > 0; numBoids--)
+        {
+            Boid a = Boid();
+            boids.Add(a);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Code/Boid.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boid : MonoBehaviour
+{
+    public Boid()
+    {
+
+    }
+
+    private void OnTriggerEnter()
+    {
+        Vector3 pos = someRenderer.transform.position;
+        Bounds bounds = myBoxCollider.bounds;
+        bool rendererIsInsideTheBox = bounds.Contains(pos);
+    }
+
+    private RaycastHit GetRaycast()
+    {
+        RaycastHit hit;
+        Ray ray = Physics.Raycast(this.gameObject.transform.position, direction, maxDistance = Mathf.Infinity, , QueryTriggerInteraction.UseGlobal);
+        Physics.Raycast(ray, out hit);
+        return hit;
+    }
+}
